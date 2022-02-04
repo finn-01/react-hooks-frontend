638e80bb35c56c410771bbb5c999642be65e20e2
finn-01 <nguyensam576@gmail.com>
2022-02-05 01:51:19 +0700

    giao dien AddEmployeeComponent.jsx

--- src/components/AddEmployeeComponent.jsx
+++ src/components/AddEmployeeComponent.jsx
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+
+import EmployeeService from "../services/EmployeeService";
 
 const AddEmployeeComponent = () => {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [emailId, setEmailId] = useState("");
+	// const navigate = useNavigate()
+	// const {id} = useParams()
+
+	const saveEmployee = (e) => {
+		e.preventDefault();
+
+		const employee = { firstName, lastName, emailId };
+
+		console.log(employee);
+	};
+
 	return (
 		<div>
-			<h1 className="text-center">Add Employee Component Called</h1>
+			<br />
+			<div className="container">
+				<div className="row">
+					<div className="card col-md-6 offset-md-3 offset-md-3">
+						<h2 className="text-center">Add Employee</h2>
+						<div className="card-body">
+							<form action="">
+								<div className="form-group mb-2">
+									<label htmlFor="" className="form-label">
+										First Name:{" "}
+									</label>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Enter First Name..."
+										name="firstName"
+										value={firstName}
+										onChange={(e) => setFirstName(e.target.value)}
+									/>
+								</div>
+								<div className="form-group mb-2">
+									<label htmlFor="" className="form-label">
+										Last Name:{" "}
+									</label>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Enter Last Name..."
+										name="lastName"
+										value={lastName}
+										onChange={(e) => setLastName(e.target.value)}
+									/>
+								</div>
+								<div className="form-group mb-2">
+									<label htmlFor="" className="form-label">
+										Email:{" "}
+									</label>
+									<input
+										type="email"
+										className="form-control"
+										placeholder="Enter Email..."
+										name="emailId"
+										value={emailId}
+										onChange={(e) => setEmailId(e.target.value)}
+									/>
+								</div>
+
+								<button
+									className="btn btn-success"
+									onClick={(e) => saveEmployee(e)}
+								>
+									Submit
+								</button>
+								<Link to="/employees" className="btn btn-danger">
+									Cancel
+								</Link>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
